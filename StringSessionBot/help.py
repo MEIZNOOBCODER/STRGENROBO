8c3990776d94752d263e14bac1b215c2db3bca50
Anonymous Boy <rahulking25102006@gmail.com>
2021-12-12 03:17:39 +0530

    Update help.py

--- StringSessionBot/help.py
+++ StringSessionBot/help.py
@@ -8,6 +8,6 @@ from pyrogram.types import InlineKeyboardMarkup
 async def _help(bot, msg):
     await bot.send_message(
         msg.chat.id,
-        "**Here's how to use me **\n" + Data.HELP,
+        "**ʜᴇʀᴇ's ᴛʜᴇ ɢᴜɪᴅᴇ ᴛᴏ ᴋɴᴏᴡ ᴜsɪɴɢ ᴍᴇ.**\n" + Data.HELP,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons)
     )
