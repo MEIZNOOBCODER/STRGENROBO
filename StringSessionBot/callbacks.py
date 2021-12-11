64052190fe7e51ed866cf2d9469a8f8c35792b02
Anonymous Boy <rahulking25102006@gmail.com>
2021-12-12 03:12:50 +0530

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -37,13 +37,13 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
         await bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
-            text="**Here's How to use me**\n" + Data.HELP,
+            text="**ʜᴇʀᴇ's ᴛʜᴇ ɢᴜɪᴅᴇ ᴛᴏ ᴋɴᴏᴡ ᴜsɪɴɢ ᴍᴇ.**\n" + Data.HELP,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
     elif query == "generate":
         await callback_query.message.reply(
-            "Please choose the python library you want to generate string session for",
+            "ᴘʟᴇᴀsᴇ ᴄʜᴏᴏsᴇ ᴛʜᴇ ᴘʏᴛʜᴏɴ ʟɪʙʀᴀʀʏ ꜰᴏʀ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ sᴛʀɪɴɢ ꜰᴏʀ :-",
             reply_markup=InlineKeyboardMarkup([[
                 InlineKeyboardButton("Pyrogram", callback_data="pyrogram"),
                 InlineKeyboardButton("Telethon", callback_data="telethon")
